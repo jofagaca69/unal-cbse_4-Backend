d7bc32f6a3db78d1324d85369b437ee1f137de40
José García <jofagaca69@gmail.com>
2024-11-01 13:45:33 -0500

    Feat: Ajustes en lógica de puertos

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3001);
+  await app.listen(parseInt(process.env.PORT, 10) || 3001);
 }
 
 bootstrap();
